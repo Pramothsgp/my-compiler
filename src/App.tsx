edb67e996081aeb129dc5e4f22ec5d8ae53b0048
sakthi <sakthipalanisamy7@gmail.com>
2025-03-20 10:36:54 +0530

    routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { createContext, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import MainPage from "./components/MainPage";
 import questions from "./data/sampleQuestion";
+import Login from "./pages/auth/Login";
+import PreTestPage from "./pages/PreTestPage";
 export const AppContext = createContext<any>(undefined);
 
 function App() {
@@ -10,7 +13,11 @@ function App() {
   return (
     <>
       <AppContext.Provider value={{ user, setUser, question, setQuestion }}>
-        <MainPage />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/start-test" element={<PreTestPage />} />
+          <Route path="/test" element={<MainPage />} />
+        </Routes>
       </AppContext.Provider>
     </>
   );
