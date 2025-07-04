05e7d168e945670e057b844b9c4f8ec6122a3c17
Pramothsgp <jarvis.tech.006@gmail.com>
2025-07-05 00:48:58 +0530

    Refactor ProtectedRoute to manage login state using sessionStorage instead of AuthContext

--- src/context/ProtectedRoute.tsx
+++ src/context/ProtectedRoute.tsx
@@ -1,12 +1,18 @@
-import React, { useContext } from 'react'
+import React, { useMemo, useState } from 'react'
 import { Navigate, Outlet } from 'react-router-dom';
-import { AuthContext } from './AuthContext';
 
 
 const ProtectedRoute: React.FC = () => {
-  const auth = useContext(AuthContext);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useMemo(() => {
+    if (sessionStorage.getItem('email')) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
   // If context is undefined or user is not logged in, redirect to login
-  if (!auth || !auth.email) {
+  if (!isLoggedIn) {
     return <Navigate to="/auth/login" replace />;
   }
   // If logged in, render the child routes
