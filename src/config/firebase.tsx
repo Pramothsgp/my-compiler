bea4a07ab4395d5ba9b621cc131c72224cc04f7e
Pramothsgp <jarvis.tech.006@gmail.com>
2025-07-05 00:48:43 +0530

    Update .gitignore to include .env file and refactor firebase configuration to use environment variables

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -1,24 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBi2hlelj868661p7RohY_pjCD9PspoWLQ",
-//   authDomain: "jumble-code-app.firebaseapp.com",
-//   projectId: "jumble-code-app",
-//   storageBucket: "jumble-code-app.firebasestorage.app",
-//   messagingSenderId: "908897698826",
-//   appId: "1:908897698826:web:ae6254ca16b6b91b470bf3",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const db = getFirestore(app);
-
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -28,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAZP548cEiYas-3yRDA1ltO38OszE_M83E",
-  authDomain: "test-f6397.firebaseapp.com",
-  projectId: "test-f6397",
-  storageBucket: "test-f6397.firebasestorage.app",
-  messagingSenderId: "1033820175178",
-  appId: "1:1033820175178:web:ec7850d5d9db93113c1fb3",
-  measurementId: "G-MEFY5XFYFS"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
