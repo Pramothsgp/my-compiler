de4e5cfcdc2776d116ab5ab203dc714157b4bbb9
Pramothsgp <jarvis.tech.006@gmail.com>
2025-07-04 21:39:52 +0530

    Update firebase.tsx: update Firebase config and initialization

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -1,3 +1,24 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBi2hlelj868661p7RohY_pjCD9PspoWLQ",
+//   authDomain: "jumble-code-app.firebaseapp.com",
+//   projectId: "jumble-code-app",
+//   storageBucket: "jumble-code-app.firebasestorage.app",
+//   messagingSenderId: "908897698826",
+//   appId: "1:908897698826:web:ae6254ca16b6b91b470bf3",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -5,13 +26,15 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBi2hlelj868661p7RohY_pjCD9PspoWLQ",
-  authDomain: "jumble-code-app.firebaseapp.com",
-  projectId: "jumble-code-app",
-  storageBucket: "jumble-code-app.firebasestorage.app",
-  messagingSenderId: "908897698826",
-  appId: "1:908897698826:web:ae6254ca16b6b91b470bf3",
+  apiKey: "AIzaSyAZP548cEiYas-3yRDA1ltO38OszE_M83E",
+  authDomain: "test-f6397.firebaseapp.com",
+  projectId: "test-f6397",
+  storageBucket: "test-f6397.firebasestorage.app",
+  messagingSenderId: "1033820175178",
+  appId: "1:1033820175178:web:ec7850d5d9db93113c1fb3",
+  measurementId: "G-MEFY5XFYFS"
 };
 
 // Initialize Firebase
